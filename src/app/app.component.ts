1efc095c4e392857966e45b61a8c13352952d93c
himanshu@webtheuniverse.com <himanshu@webtheuniverse.com>
2022-08-08 17:25:33 +0530

    done advan angu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserdataService } from './services/userdata.service';
+import { ApiDataService } from './services/api-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'advanceAngu';
+  userDetail:any;
+  users:any;
+
+  constructor(private userdata: UserdataService, private apiData: ApiDataService) {
+      console.log("userData", userdata.users());
+      this.userDetail = userdata.users();
+      // api -2
+      apiData.users().subscribe((data:any)=> {
+        console.log(data);
+        this.users = data;
+      })
+  }
+  getUserData(item: string) {
+     console.log("item",item); 
+     this.apiData.saveUser(item).subscribe((result) => {
+      console.log("new", result);
+      
+     });
+  }
 }
